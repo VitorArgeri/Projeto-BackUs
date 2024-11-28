da75ef7a63adf8326601bc930241f78eb38ff212
VitorArgeri <vitor.argeri@aluno.senai.br>
2024-11-28 15:11:50 -0300

    fix: roleta.js and listaalunos.js

--- ETAPA 2/ListaAlunos.js
+++ ETAPA 2/ListaAlunos.js
@@ -1,63 +1,11 @@
 import { iniciarJogo } from './Roleta.js';
 
-let alunos = [
-    {
-        id: 0,  
-        nome: 'Vitor Argeri',
-        sexo: 'Masculino',
-        idade: '17',
-        altura: '173',
-        jogar: true,
-        funcao: '',  
-        morto: true,
-        ganhou: true
-    },
-    {
-        id: 0,
-        nome: 'Miguel Sarti',
-        sexo: 'Masculino',
-        idade: '16',
-        altura: '180',
-        jogar: true,
-        funcao: '', 
-        morto: true,
-        ganhou: false,
-    },
-    {
-        id: 0,
-        nome: 'Vinícius Pereira',
-        sexo: 'Masculino',
-        idade: '17',
-        altura: '170',
-        jogar: true,
-        funcao: '', 
-        morto: true,
-        ganhou: false,
-    },
-    {
-        id: 0,
-        nome: 'Vinícius Rocha',
-        sexo: 'Masculino',
-        idade: '17',
-        altura: '178',
-        jogar: false,
-        funcao: '',  
-        morto: false,
-        ganhou: false,
-    },
-    {
-        id: 0,
-        nome: 'Pablo Delgado',
-        sexo: 'Masculino',
-        idade: '16',
-        altura: '183',
-        jogar: true,
-        funcao: '',  
-        morto: false,
-        ganhou: true,
-    }
+const alunos = [
+  { nome: 'Vitor Argeri', sexo: 'Masculino', idade: '17', altura: '173', jogar: true, morto: true, ganhou: true },
+  { nome: 'Miguel Sarti', sexo: 'Masculino', idade: '16', altura: '180', jogar: true, morto: true, ganhou: false },
+  { nome: 'Vinícius Pereira', sexo: 'Masculino', idade: '17', altura: '170', jogar: true, morto: true, ganhou: false },
+  { nome: 'Vinícius Rocha', sexo: 'Masculino', idade: '17', altura: '178', jogar: false, morto: false, ganhou: false },
+  { nome: 'Pablo Delgado', sexo: 'Masculino', idade: '16', altura: '183', jogar: true, morto: false, ganhou: true }
 ];
 
-alunos = iniciarJogo(alunos);
-
-export default alunos;
+export default iniciarJogo(alunos);
--- ETAPA 2/Roleta.js
+++ ETAPA 2/Roleta.js
@@ -2,11 +2,13 @@ const generateID = () => Math.floor(Math.random() * 10000);
 
 const roletaDeFuncoes = () => ['Inocente', 'Sabotador'][Math.floor(Math.random() * 2)];
 
-const iniciarJogo = (alunos) => alunos.map(aluno => ({
-  ...aluno,
-  id: generateID(),
-  funcao: roletaDeFuncoes(),
-  papel: 'jogador'
-}));
+const iniciarJogo = (alunos) => alunos.map(aluno => (
+  aluno.jogar ? {
+    ...aluno,
+    id: generateID(),
+    funcao: roletaDeFuncoes(),
+    papel: 'jogador'
+  } : aluno
+));
 
 export { iniciarJogo, generateID };
